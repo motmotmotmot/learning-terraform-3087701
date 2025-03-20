7bda75d5e4270583afe6f5dbc18f3b9888ffea48
motmotmotmot <79494464+motmotmotmot@users.noreply.github.com>
2025-03-20 15:38:24 -0500

    Modules

--- main.tf
+++ main.tf
@@ -34,47 +34,10 @@ module "blog_sg" {
   version = "5.3.0"
   name    = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow HTTP/s In and allow everything out"
-
   vpc_id              = data.aws_vpc.default.id
   ingress_rules       = ["http-80-tcp","http-443-tcp"]
   ingress_cidr_blocks = ["73.62.218.0/24"]
 
   egress_rules  = ["all-all"]
   cidr_blocks   = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["73.62.218.0/24"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["73.62.218.0/24"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
 }
